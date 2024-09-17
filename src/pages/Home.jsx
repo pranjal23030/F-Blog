0288990c6eb225f7bfb3bd780fe112cb79715f1b
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-09-17 14:07:09 +0545

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import Card from "../components/Card";
+import Navbar from "../components/Navbar";
+import axios from "axios";
+
+function Home() {
+  const fetchBlogs = async () => {
+    const response = await axios.get("http://localhost:3000/blog");
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-wrap">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  );
+}
+
+export default Home;
