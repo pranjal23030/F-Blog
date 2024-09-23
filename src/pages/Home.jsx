1ec57a834a788fb5e8984e034a6fe1c0d355b770
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-09-23 18:12:39 +0545

    code-formatted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,33 @@
-import { useEffect, useState } from "react"
-import Card from "../components/Card"
-import Navbar from "../components/Navbar"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import Card from "../components/Card";
+import Navbar from "../components/Navbar";
+import axios from "axios";
 
+function Home() {
+  const [blogs, setBlogs] = useState([]);
+  const fetchBlogs = async () => {
+    const response = await axios.get("http://localhost:3000/blog");
+    setBlogs(response.data.data);
+  };
 
-function Home(){
-    const [blogs,setBlogs] = useState([])
-    const fetchBlogs = async ()=>{
-      const response =  await axios.get("http://localhost:3000/blog")
-      setBlogs(response.data.data)
-    }
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+  console.log(blogs);
 
-    useEffect(()=>{
-        fetchBlogs()
-    },[])
-    console.log(blogs)
-
-   
-    return(    
-        <>
-            <Navbar/>
-            <div className="flex flex-wrap">
-            {
-                blogs.map(function(blog){
-                    return(
-                        // eslint-disable-next-line react/jsx-key
-                        <Card blog={blog} />
-                    )
-                })
-            }
-     
-
-            </div>
-
-        </>
-
-
-    )
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-wrap">
+        {blogs.map(function (blog) {
+          return (
+            // eslint-disable-next-line react/jsx-key
+            <Card blog={blog} />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
