ea8c58bff903cadae9f9792f6a47010999141f85
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-09-23 19:59:40 +0545

    navbar-header-name-updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            Blog
           </span>
         </a>
         <button
