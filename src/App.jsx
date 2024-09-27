4ba8c3a9fe7f82f08cb6f1a62c7d72d01ecf89b1
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-09-27 14:37:01 +0545

    edit-route-added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SingleBlog from "./pages/SingleBlog";
 import CreateBlog from "./pages/CreateBlog";
+import EditBlog from "./pages/EditBlog";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<SingleBlog />} />
-        <Route path="create" element={<CreateBlog />} />
+        <Route path="/create" element={<CreateBlog />} />
+        <Route path="/edit/:id" element={<EditBlog />} />
       </Routes>
     </BrowserRouter>
   );
