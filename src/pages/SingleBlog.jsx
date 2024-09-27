5d225b371145e08d272e275040ec00602cace3d9
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-09-27 14:36:07 +0545

    link-to-edit-blog

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 function SingleBlog() {
   const { id } = useParams();
@@ -42,9 +42,11 @@ function SingleBlog() {
               </div>
               <div className="flex -mx-2 mb-4">
                 <div className="w-1/2 px-2">
-                  <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">
-                    Edit me
-                  </button>
+                  <Link to={`/edit/${blog._id}`}>
+                    <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">
+                      Edit me
+                    </button>
+                  </Link>
                 </div>
                 <div className="w-1/2 px-2">
                   <button
