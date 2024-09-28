940e3feaa76af27fc14c954d9ef763f13a5f4294
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-09-28 16:48:03 +0545

    project-name-changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Blog
+            Blog Project
           </span>
         </Link>
         <button
